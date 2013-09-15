9e40a404f3581dccc87c554b67b6b267dfcaac19
EJ Bensing <ebensing@rm-dash-r.com>
2013-09-15 13:56:35 -0700

    updated the script for new dev workflow

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 These are a collection of some random scripts for the JDA site that might be useful
 
-* createNewDev.sh - to use this, pass in a username for which you want to create a dev environment (IE. `./createNewDev.sh ej`) - NOTE: You will still need to edit the parameters.ini that gets copied into app/config/
+* createNewDev.sh - to use this, pass in a username for which you want to
+* create a dev environment (IE. `./createNewDev.sh`) - NOTE: You will still
+* need to edit the parameters.ini that gets copied into app/config/
--- createNewDev.sh
+++ createNewDev.sh
@@ -2,14 +2,22 @@
 
 cd /var/www
 
-git clone git@github.com:Japan-Digital-Archives/Japan-Digital-Archive.git $1
+echo "Please enter the username(on the server) of the user: "
+read username
 
-cd $1
+echo "Please enter the username(on Github) of the user: "
+read githubUsername
+
+git clone git@github.com:Japan-Digital-Archives/Japan-Digital-Archive.git $username
+
+cd $username
 
 git fetch origin stable
 git checkout stable
-git checkout -b $1
-git push origin $1
+git remote rm origin
+git remote add origin "git@github.com:$githubUsername/Japan-Digital-Archive.git"
+git checkout -b $username
+git push -u origin $username
 
 cp ../ej/app/config/parameters.ini app/config/
 
