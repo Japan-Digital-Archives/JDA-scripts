3abb286f4f754b537699a240e51344822395256b
horak <alexamh9@gmail.com>
2016-02-08 15:35:16 -0500

    trim new lines from 'attribution', fix existence error within location, fix json format, separate out attribution uri and uri, fix child_items_count and published

--- xml_spider_kahoku.py
+++ xml_spider_kahoku.py
@@ -5,8 +5,9 @@ from scrapy.http import Request
 class XmlSpider(BaseSpider):
   name = "xmlscrape"
   allowed_domains = ["kahoku-archive.shinrokuden.irides.tohoku.ac.jp"]
+    # "http://kahoku-archive.shinrokuden.irides.tohoku.ac.jp/webapi/oaipmh?verb=ListRecords&metadataPrefix=sdn&set=IMAGE"
   start_urls = [
-    "http://kahoku-archive.shinrokuden.irides.tohoku.ac.jp/webapi/oaipmh?verb=ListRecords&metadataPrefix=sdn&set=IMAGE"
+    "http://kahoku-archive.shinrokuden.irides.tohoku.ac.jp/webapi/oaipmh?verb=ListRecords&metadataPrefix=sdn&set=IMAGE&resumptionToken=5WR5-HYweU6yQfHFWgiqTw"
   ]
 
   def handleNull(self, field):
@@ -18,28 +19,30 @@ class XmlSpider(BaseSpider):
 
   def parse(self, response):
     x = XmlXPathSelector(response)
+    # y = XmlXPathSelector(response)
+    # y.remove_namespaces()
     x.remove_namespaces()
     x.register_namespace("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
     items = []
     items = x.select('//record/metadata/RDF')
+    # itemsY = y.select('//record/header/@status')
+    # print '**************** ', itemsY
 
     jsons = []
+    num = 0
 
+    # maybe the problem is just the number of item
     for item in items:
       ####################
       ##### creator ######
       ##### archive ######
       #### media_type ####
       #### layer_type ####
-      # child_items_count 
-      #### published #####
       ####################
       media_creator_username = 'Kahoku Simpo Publishing Co.'
       archive = "Kahoku Shimpo Disasters Archive"
       media_type = "Image"
       layer_type = "Image" 
-      child_items_count = '0'
-      published = '1'
 
       ####################
       # media_date_created
@@ -54,23 +57,29 @@ class XmlSpider(BaseSpider):
       # title = item.select('Resource/title/text()').extract() # seems to be the same for everything ... so i'm using abstract instead which seems to change
       abstract = item.select('Resource/abstract/text()').extract()
       abstract = self.handleNull(abstract)
-      # print "*****************abstract ", abstract[0]
+      abstract = abstract.replace("\r\n", "")
+      # print "*****************abstract ", abstract
 
       ####################
       ####### URI ######## 
       ####################
       uri = item.select('Resource/screen/Image/@rdf:about').extract()
       uri = self.handleNull(uri)
-      attribution_uri = uri
-      # print "**********uri*************: ", uri[0]
-      # print "**********attribution_uri*************: ", attribution_uri[0]
+      # print "**********uri*************: ", uri
+
+      ####################
+      ####### URI ######## 
+      ####################
+      attribution_uri = item.select('Resource/@rdf:about').extract()
+      attribution_uri = self.handleNull(attribution_uri)
+      # print "**********attribution_uri*************: ", attribution_uri
 
       ####################
       ####### Tags ####### 
       ####################
       tags = item.select('Resource/subject/Description/value/text()').extract()
-      # print "**********tags*************: ", tags[0]
       tags = self.handleNull(tags)
+      # print "**********tags*************: ", tags
       tags_string = '"' + '", "'.join(tags) + '"'
 
       ####################
@@ -78,7 +87,8 @@ class XmlSpider(BaseSpider):
       ####################
       thumbnail_url = item.select('Resource/thumbnail/Image/@rdf:about').extract()
       thumbnail_url = self.handleNull(thumbnail_url)
-      # print "**********thumbnail*************: ", thumbnail
+      print "**********thumbnail*************: ", thumbnail_url
+      print '^ thumbnail before death'
 
       ####################
       ##### Location ##### 
@@ -86,12 +96,12 @@ class XmlSpider(BaseSpider):
       region = item.select('Resource/spatial/Description/region/text()').extract()
       locality = item.select('Resource/spatial/Description/locality/text()').extract()
       street_address = item.select('Resource/spatial/Description/street-address/text()').extract()
-
-      region = self.handleNull(region)
-      locality = self.handleNull(locality)
-      street_address = self.handleNull(street_address)
-      locationTemp = [street_address, locality, region]
-      location = ''
+      if region or locality or street_address:
+        region = self.handleNull(region)
+        locality = self.handleNull(locality)
+        street_address = self.handleNull(street_address)
+        locationTemp = [street_address, locality, region]
+        location = ''
 
       # This handles comma location and existence issues
       for item in locationTemp:
@@ -104,7 +114,8 @@ class XmlSpider(BaseSpider):
         location = location[:-1]
       # print "**********location*************: ", location
 
-      json_entry = ( '{"title": "' + abstract[0] + '", "uri": "' 
+      json_entry = ( '{"title": "' 
+        + abstract + '", "uri": "' 
         + uri + '", "attribution_uri": "' 
         + attribution_uri + '", "media_creator_username": "' 
         + media_creator_username + '", "thumbnail_url": "' 
@@ -113,11 +124,10 @@ class XmlSpider(BaseSpider):
         + tags_string + '], "archive": "' 
         + archive + '",  "media_type": "'
         + media_type + '", "layer_type": "'
-        + layer_type + '", "child_items_count": "'
-        + child_items_count + '", "published": "'
-        + published + '"}, '
+        + layer_type + '", "child_items_count": 0, "published": 1}, '
       )
 
+      # print json_entry
       jsons.append(json_entry)
 
     resumptionToken = x.select('//resumptionToken/text()').extract()
