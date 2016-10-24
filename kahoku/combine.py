af9934d81142b9874aa6363eb912e08b3eca42ee
Horak-temp <horak@Chelseas-MacBook-Pro.local>
2016-10-24 19:13:20 +1000

    Combine files and format properly

--- kahoku/combine.py
+++ kahoku/combine.py
@@ -1,32 +1,55 @@
-#################################################
-# This script is for concatenating the contents
-# of the scraped contents of the Kahoku Shimpo
-# Disaster Archive.
-#################################################
-import glob, sys, argparse
+#!/usr/bin/env python
 
-p = argparse.ArgumentParser(description='loop.py')
+###########
+# Imports #
+###########
+import glob, sys, argparse, time, os
+
+#######
+# CLI #
+#######
+p = argparse.ArgumentParser(description='combine.py')
 p.add_argument('-l', dest='location')
-
 args = p.parse_args()
 legal = ['image', 'document', 'movie', 'other']
 
-# Handle Errors
+#################
+# Handle Errors #
+##################
 if args.location == None:
     print 'Must indicate a location: ie: image, document, movie, other.'
     sys.exit(1)
+
 if args.location not in legal:
     print 'Please specify a legal location. ie: image, document, movie, other.'
     sys.exit(1)
 
-
+########
+# Main #
+########
 if args.location:
-	path = '/Users/horak/Dropbox/JDA/JDA-scripts/kahoku/' + args.location + '_output/'
+	timestr    = time.strftime('%Y%m%d-%H%M%S')
+	read_path  = '/Users/horak/JDA-scripts/kahoku/' + args.location + '_output/'
+	result_path = read_path + 'combined-' + timestr + '.json'
 
-	read_files = glob.glob(path + '*.json')
-	with open(path + 'merged_file.json', 'wb') as outfile:
-	    outfile.write('[{}]'.format(
+	# Combine Files
+	read_files = glob.glob(read_path + '*.json')
+	with open(result_path, 'wb') as outfile:
+	    outfile.write('{}'.format(
 	        ' '.join([open(f, "rb").read() for f in read_files])))
+	
+	# Prepend "Items" string
+	with file(result_path, 'r') as original: data = original.read()
+	with file(result_path, 'w') as modified: modified.write('{"items": [' + data)
+
+	# Remove hanging comma
+	with open(result_path, 'rb+') as modified:
+		modified.seek(-2, os.SEEK_END)
+		modified.truncate()
+
+	# Append end of file formatting
+	with open(result_path, 'a') as modified: modified.write(']}')
+
 else:
     print 'Unpredicted error.'
     sys.exit(1)
